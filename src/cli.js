b3afbde5f5d91ce9b422d4dad43170d0a3d7c8e5
Ilya Provotorov <arsael@yandex.ru>
2023-03-21 08:03:52 +0300

    Cli.js and brain-games.js refactored

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,9 @@
-import{question} from "readline-sync";
-// Wait for user's response.
-export let userName = question('May I have your name? ');
+import readlineSync from 'readline-sync';
+//Getting userName and greeting him with welcome message
+export const getUserName = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+    return userName;
+}
+
